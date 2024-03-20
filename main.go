901fc226f770bde2a92cad5845a2afb627d06358
dmcgeer <devan@rezco.co.za>
2024-03-20 15:56:29 +0200

    Added bit manipulation
    Added ascii array
    
    Added write file method

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func init() {
 
 func main() {
 
-	file, err := os.Open("self.jpeg")
+	file, err := os.Open("image.jpeg")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -39,11 +39,41 @@ func main() {
 
 	img, _, err := image.Decode(file)
 
+	destinationFile, err := os.Create("ascii.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer destinationFile.Close()
+
 	fmt.Println(img.At(10, 10).RGBA())
 	for y := range height {
 		for x := range width {
-			fmt.Println(img.At(x, y).RGBA())
-			fmt.Printf("[%d, %d] ", x, y)
+			r, g, b, a := img.At(x, y).RGBA()
+			brightness := caclulateBrightness(r, g, b, a)
+			char := getAsciiChar(brightness)
+			destinationFile.WriteString(char)
 		}
+		destinationFile.WriteString("\n")
 	}
+	fmt.Println("Done")
+}
+
+func caclulateBrightness(r, g, b, a uint32) uint32 {
+	r = (r >> 8) & 0xFF
+	g = (g >> 8) & 0xFF
+	b = (b >> 8) & 0xFF
+	a = (a >> 8) & 0xFF
+	return (r + g + b + a) / 4
+}
+
+func getAsciiChar(brightness uint32) string {
+	asciiPixels := []string{"_", ".", ":", "-", "=", "+", "*", "#", "%", "@"}
+	amountOfPixels := len(asciiPixels)
+
+	pixel := float32(brightness) / 255 * float32(amountOfPixels-1)
+	var pix int = int(pixel)
+
+	fmt.Println(pix)
+	return asciiPixels[pix]
 }
